6b78972f3a56d6bb9af80e4362163049fffd4c0b
DhritimanT <dhritimant@gmail.com>
2025-10-22 01:01:08 +0530

    Remove AWS Amplify setup documentation and package-lock.json; update next.config.js for static export configuration

--- next.config.js
+++ next.config.js
@@ -1,6 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export',
   reactStrictMode: true,
   images: {
     domains: ['via.placeholder.com', 'devapi.vidyaai.co', 'api.vidyaai.co'],
